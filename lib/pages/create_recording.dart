6825d9be964a22948b36b290dcf47fb947e2a1a0
Sean <dev@nyayozangu.com>
2018-11-11 21:46:48 +0300

    chipkizi v0.0.8 record ui - tweaking rocord page UI

--- lib/pages/create_recording.dart
+++ lib/pages/create_recording.dart
@@ -27,35 +27,74 @@ class RecordingPage extends StatelessWidget {
         ));
 
     _handleRecording(BuildContext context, MainModel model) {
-      model.isRecording ? model.stopRecording() : model.startRecording();
-
-      if (!model.isRecording)
-        Scaffold.of(context).showBottomSheet((context) {
-          return _buildPlayerControls(model);
-        });
+      switch (model.isRecording) {
+        case true:
+          model.stopRecording();
+          Scaffold.of(context).showBottomSheet((context) {
+            return _buildPlayerControls(model);
+          });
+          break;
+        case false:
+          model.startRecording();
+          break;
+      }
     }
 
     final _bodySection =
         ScopedModelDescendant<MainModel>(builder: (_, __, model) {
-      return Column(children: <Widget>[
-        model.isRecording ? Text(model.recorderTxt) : Text(model.playerText),
-        Center(
-          child: Builder(
-            builder: (context) {
-              return IconButton(
-                icon: model.isRecording ? Icon(Icons.stop) : Icon(Icons.mic),
-                onPressed: () => _handleRecording(context, model),
-              );
-            },
-          ),
-        ),
-      ]);
+      return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            model.isRecording
+                ? Text(
+                    model.recorderTxt,
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  )
+                : Text(
+                    model.playerText,
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+            Center(
+              child: Builder(
+                builder: (context) {
+                  return Material(
+                    shape: CircleBorder(),
+                    color: Colors.brown,
+                    elevation: 4.0,
+                    child: Container(
+                      height: 150.0,
+                      width: 150.0,
+                      decoration: BoxDecoration(shape: BoxShape.circle),
+                      child: IconButton(
+                        icon: model.isRecording
+                            ? Icon(
+                                Icons.stop,
+                                color: Colors.white,
+                                size: 80.0,
+                              )
+                            : Icon(
+                                Icons.mic,
+                                color: Colors.white,
+                                size: 80.0,
+                              ),
+                        onPressed: () => _handleRecording(context, model),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ]);
     });
 
     return Scaffold(
       appBar: AppBar(
+        elevation: 0.0,
         title: Text(APP_NAME),
       ),
+      backgroundColor: Colors.brown,
       body: _bodySection /*RecordingBody()*/,
     );
   }
