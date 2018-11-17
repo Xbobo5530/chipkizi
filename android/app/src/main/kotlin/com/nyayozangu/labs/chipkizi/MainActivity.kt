72b9e70a8158fa0184510f7db0e026902f4b1268
Sean <dev@nyayozangu.com>
2018-11-17 20:55:27 +0300

    __v50 - added splach screen

--- android/app/src/main/kotlin/com/nyayozangu/labs/chipkizi/MainActivity.kt
+++ android/app/src/main/kotlin/com/nyayozangu/labs/chipkizi/MainActivity.kt
@@ -4,10 +4,25 @@ import android.os.Bundle
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
+import android.view.WindowManager;
+import android.view.ViewTreeObserver
+
+
 
 class MainActivity(): FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     GeneratedPluginRegistrant.registerWith(this)
+
+    //Remove full screen flag after load
+    val vto = getFlutterView().getViewTreeObserver()
+    vto.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener {
+      override fun onGlobalLayout() {
+        getFlutterView().getViewTreeObserver().removeOnGlobalLayoutListener(this)
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+      }
+    })
+
+   
   }
-}
+  }
